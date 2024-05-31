2bfbbe8e0486b30e79a4bee75031d10d5940ed3e
김민결 <neomingyeol@gmail.com>
2024-05-31 20:41:04 +0900

    Blocking Deving

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using Unity.XR.Oculus.Input;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
@@ -15,15 +17,25 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float jumpForce;
     [SerializeField] private float turnSpeed;
-
+    [SerializeField] private LayerMask playerMask;
+    [SerializeField] private float playerDetectedRange;
     private Vector3 moveDirection;
 
     private float horizontal;
     private float vertical;
 
+    private readonly int HashTarget = Animator.StringToHash("Targeting");
+    private readonly int HashVelocityX = Animator.StringToHash("Velocity X");
+    private readonly int HashVelocityZ = Animator.StringToHash("Velocity Z");
+
+    private readonly int HashMoving = Animator.StringToHash("Moving");
+    private readonly int HashBlock = Animator.StringToHash("Blocking");
+
     private readonly int HashAttack = Animator.StringToHash("Attack");
     private readonly int HashMoveAttack = Animator.StringToHash("MoveAttack");
-    private readonly int HashMoving = Animator.StringToHash("Moving");
+    private readonly int HashPowerUpAttack = Animator.StringToHash("PowerUpAttack");
+    private readonly int HashBlockAttack = Animator.StringToHash("BlockAttack");
+    private readonly int HashRushAttack = Animator.StringToHash("RushAttack");
 
     private bool isJumping;
     private bool isMoveing;
@@ -53,9 +65,7 @@ public class Player : MonoBehaviour
         vertical = Input.GetAxisRaw("Vertical"); // W, S 
 
         isBlocking =  Input.GetKey(KeyCode.Mouse1);
-        isTargeting = Input.GetKey(KeyCode.LeftControl);
-        if (Input.GetKey(KeyCode.Mouse1)) Blocking();
-        if (Input.GetKey(KeyCode.LeftControl)) Targeting();
+        if (isBlocking) Blocking();
 
         if (Input.GetKeyDown(KeyCode.E)) Skill1();
         if (Input.GetKeyDown(KeyCode.Q)) Skill2();
@@ -66,15 +76,19 @@ public class Player : MonoBehaviour
 
     private void SetAnimatorParameter()
     {
-        isMoveing = moveDirection != Vector3.zero;
         animator.SetBool(HashMoving, isMoveing);
+        animator.SetBool(HashBlock, isBlocking);
     }
 
     private void MoveRotate()
     {
         // 이동방향으로 회전
-        if (moveDirection != Vector3.zero)
+        if (isMoveing && !isBlocking)
+        {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDirection), Time.deltaTime * turnSpeed);
+
+        }
+            
     }
 
     private void SetDirection()
@@ -93,11 +107,7 @@ public class Player : MonoBehaviour
 
         // 입력에 따라 이동 방향 계산
         moveDirection = forward * vertical + right * horizontal;
-    }
-
-    private void Targeting()
-    {
-
+        isMoveing = moveDirection != Vector3.zero;
     }
 
     private void Jump()
@@ -123,11 +133,34 @@ public class Player : MonoBehaviour
 
     private void Blocking()
     {
+        Collider[] otherPlayerColl = GetOtherPlayerColl();
+        isTargeting = otherPlayerColl.Length > 1;
 
+        if (isMoveing && isTargeting)
+        {
+            animator.SetFloat(HashVelocityX, moveDirection.x);
+            animator.SetFloat(HashVelocityZ, moveDirection.z);
+
+            Vector3 lookPlayerDir = transform.position - otherPlayerColl[1].transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookPlayerDir), Time.deltaTime * turnSpeed);
+
+        }
+        else if (isMoveing)
+        {
+            animator.SetFloat(HashVelocityZ, moveDirection.z);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDirection), Time.deltaTime * turnSpeed);
+
+        }
+    }
+
+    private Collider[] GetOtherPlayerColl()
+    {
+        return Physics.OverlapSphere(transform.position, playerDetectedRange, playerMask);
     }
 
     private void Attack()
     {
-
+        animator.SetTrigger(HashAttack);
     }
 }
