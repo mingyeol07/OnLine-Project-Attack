236e44c3ff8d85a0f230b0c427e82ec731ca5674
Mingyeol Kim <neomingyeol@gmail.com>
2024-06-03 00:29:08 +0900

    let's go backend

--- Assets/Scripts/Backend/BackendLogin.cs
+++ Assets/Scripts/Backend/BackendLogin.cs
@@ -7,5 +7,33 @@ using UnityEngine;
 
 public class BackendLogin : MonoBehaviour
 {
+    private static BackendLogin _instance = null;
 
+    public static BackendLogin Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new BackendLogin();
+            }
+
+            return _instance;
+        }
+    }
+
+    public void CustomSignUp(string id, string pw)
+    {
+        // Step 2. 회원가입 구현하기 로직
+    }
+
+    public void CustomLogin(string id, string pw)
+    {
+        // Step 3. 로그인 구현하기 로직
+    }
+
+    public void UpdateNickname(string nickname)
+    {
+        // Step 4. 닉네임 변경 구현하기 로직
+    }
 }
